949a8b564726ebe8a02a7a1b0c77ebfb22bd8cf9
sofidorosh <108129293+anenkosofi@users.noreply.github.com>
2023-01-09 17:36:38 +0200

    Deleted comment, rewrite functions

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,24 +4,11 @@ import { nanoid } from 'nanoid';
 import { Form, Field, Label, Input, Button } from './ContactForm.styled';
 
 export function ContactForm({ onSubmit }) {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+  const [form, setForm] = useState({ name: '', number: '' });
 
   const handleChange = e => {
     const { name, value } = e.currentTarget;
-
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-
-      case 'number':
-        setNumber(value);
-        break;
-
-      default:
-        return;
-    }
+    setForm(prevForm => ({ ...prevForm, [name]: value }));
   };
 
   const handleSubmit = e => {
@@ -29,14 +16,12 @@ export function ContactForm({ onSubmit }) {
 
     const newContact = {
       id: nanoid(),
-      name: name,
-      number: number,
+      ...form,
     };
 
     onSubmit(newContact);
 
-    setName('');
-    setNumber('');
+    setForm({ name: '', number: '' });
   };
 
   return (
@@ -50,7 +35,7 @@ export function ContactForm({ onSubmit }) {
           placeholder=" "
           required
           onChange={handleChange}
-          value={name}
+          value={form.name}
         />
         <Label>Name</Label>
       </Field>
@@ -63,7 +48,7 @@ export function ContactForm({ onSubmit }) {
           placeholder=" "
           required
           onChange={handleChange}
-          value={number}
+          value={form.number}
         />
         <Label>Number</Label>
       </Field>
@@ -71,65 +56,6 @@ export function ContactForm({ onSubmit }) {
     </Form>
   );
 }
-
-// export class ContactForm extends React.Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleChange = ({ currentTarget: { name, value } }) => {
-//     this.setState({ [name]: value });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-
-//     const newContact = {
-//       id: nanoid(),
-//       name: this.state.name,
-//       number: this.state.number,
-//     };
-
-//     this.props.onSubmit(newContact);
-
-//     this.setState({ name: '', number: '' });
-//   };
-
-//   render() {
-//     return (
-//       <Form onSubmit={this.handleSubmit} autoComplete="off">
-//         <Field>
-//           <Input
-//             type="text"
-//             name="name"
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             placeholder=" "
-//             required
-//             onChange={this.handleChange}
-//             value={this.state.name}
-//           />
-//           <Label>Name</Label>
-//         </Field>
-//         <Field>
-//           <Input
-//             type="tel"
-//             name="number"
-//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//             placeholder=" "
-//             required
-//             onChange={this.handleChange}
-//             value={this.state.number}
-//           />
-//           <Label>Number</Label>
-//         </Field>
-//         <Button type="submit">Add contact</Button>
-//       </Form>
-//     );
-//   }
-// }
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
